3ad7cd7a6b85f3a05f2c1aa32555df82c93490e6
ballro <123340320+ballro@users.noreply.github.com>
2023-10-07 09:32:36 -0400

    Added hello function

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void hello(int boy) ;
+
 int main(void){
   printf("Hello World") ;
+  
+  hello(5);
   return 0 ;
+}
+
+void hello(int boy)
+{
+	printf("%d", boy);
+	return;
 }
