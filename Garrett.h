adebf24c7136917f5f3f352c21e04c6937bb12a9
Garrett6419 <garrettbequette6419@gmail.com>
2023-10-07 10:47:12 -0400

    Made Tedd.h

--- Garrett.h
+++ Garrett.h
@@ -2,8 +2,13 @@ const int NUMMOVES = 8;
 const int DX[] = {-1,-1,-1,0,0,1,1,1};
 const int DY[] = {-1,0,1,-1,1,-1,0,1};
 
-checkBlocks(block input, int row, int col) {
-    for(int i = 0 ; i < row ; i++) {
-
+checkBlocks(block** array, int row, int col, int position x, int position y) {
+    for(int i = 0 ; i < NUMMOVES ; i++) {
+        if(x+DX[i] >= row || x+DX[i] < 0) {
+            continue ;
+        }
+        if(x+DX[i] >= row || x+DX[i] < 0) {
+            continue ;
+        }
     }
 }
