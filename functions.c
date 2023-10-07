51a3860410848683ce767b9d7a766fbf408ed3fc
Garrett6419 <garrettbequette6419@gmail.com>
2023-10-07 10:06:26 -0400

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,7 +1,26 @@
-#include <stdio.c>
-#include <stdlib.c>
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct block{
+  char name ;
+  int numBlocks ;
+  int numFlags ;
+}block
+
+int checkBlocks(char** array, int row, int col) ;
+int checkFlags(char** array, int row, int col) ;
+
 int main(void) {
-  printf("Hello World") ;
-  return 0 ;
+    block newBlock ;
+    newBlock.name = 'C' ;
+    numBlocks = checkBlocks(array, row, col) ;
+    numFlags = checkFlags(array, row, col)
+}
+
+int checkBlocks(char** array, int row, int col) {
+
+}
+
+int checkFlags(char** array, int row, int col) {
+
 }
